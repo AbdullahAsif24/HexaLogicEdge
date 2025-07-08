0cdd37883c62e9143a53c46f9040542518e908d8
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-07-08 12:09:46 -0700

    .

--- components/HomeSec.jsx
+++ components/HomeSec.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useState, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import AnimatedHoverText from "./AnimatedTextHover";

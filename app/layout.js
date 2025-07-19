8fa5e9464818e66226472fa110ca911f116ae35f
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-07-19 10:40:36 -0700

    .

--- app/layout.js
+++ app/layout.js
@@ -5,22 +5,19 @@ import '../styles/globals.css';
 export const metadata = {
   title: "Hexalogic",
   description: "HexaLogic - A digital agency that helps businesses grow through innovative and creative solutions.",
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-       <head>
-        <link rel="icon" href="/favicon.ico" />
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body >
-
-          {children}
-
-
+        {children}
       </body>
     </html>
   );

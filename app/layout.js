b8edad474e998d6915ff0283906f17d327e62886
AbdullahAsif24 <abdullahasif4045@gmail.com>
2025-07-19 10:33:14 -0700

    .

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
        <head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </head>
       <body >
 
